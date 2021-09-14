bf781e9a770c974cf5de1f00446e9caf44d5e216
yuno <yhkim@idealbloom.io>
2021-09-14 13:34:22 +0900

    app icon apply

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ npm i react-native-audio-recorder-player
 pod install 이후 빌드 실패시
 xcode로 프로젝트 실행
 프로젝트 클릭 후 build setting에서 excluded architectures arm64 로 적용 >> EXCLUDED_ARCHS = arm64;
+실제 디바이스에 빌드시에 다시 원상태로 돌려줘야함
 https://github.com/react-native-seoul/react-native-naver-login/issues/66
 
 [2]
