94f70b937244ea59d6f30720310180b7ff9d9ef7
yuno <yhkim@idealbloom.io>
2021-12-13 10:22:04 +0900

    pineclip 수정

--- src/Screens/PineClip/PineClipContainer.js
+++ src/Screens/PineClip/PineClipContainer.js
@@ -1,54 +1,296 @@
 import React from 'react';
+import {useEffect} from 'react';
+import {useRef} from 'react';
 import {useState} from 'react';
 import PineClipPresenter from './PineClipPresenter';
-
-const tempData = [
+const tempDataList = [
   {
-    id: '1',
-    title: 'title1',
-    category: 'washing',
-    url: 'https://youtu.be/L3kbuFsuhPI',
+    id: '14',
+    title: '블리처스(BLITZERS) ☃ 올겨울 필수 아이(돌)템 BEST7 [TMI]',
+    category: '세탁기',
+    url: 'https://youtu.be/-DfZ2UWl9ro',
   },
   {
     id: '2',
-    title: 'title2',
-    category: 'pinedoll',
-    url: 'https://youtu.be/uZGfWDU9gwQ',
+    title: '이걸 다 말해도 되는 거야? 스카이리(SKYLE) TMI',
+    category: '세탁기',
+    url: 'https://youtu.be/giMGCfay7Os',
   },
   {
+    id: '7',
+    title: '킹덤(KINGDOM) 여자가 좋아하는 남자유형 다있음 [TMI]',
+    category: '세탁기',
+    url: 'https://youtu.be/VG2mQXoRS2A',
+  },
+  {
+    id: '11',
+    title: '수능 끝났는데 남소 받을래? (기대해 9명임😊) 다크비(DKB)랑 [TMI]',
+    category: '세탁기',
+    url: 'https://youtu.be/FIImoLz-UQ4',
+  },
+  {
+    id: '1',
+    title:
+      '오늘부터 최애, 스카이리(SKYLE) FLY UP HIGH [HEART VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/58WPGNcA6DM',
+  },
+
+  {
     id: '3',
-    title: 'title3',
-    category: 'interview',
-    url: 'https://youtu.be/CuklIb9d3fI',
+    title:
+      '데뷔해줘서 고마운 ㅜㅜ 스카이리(SKYLE) FLY UP HIGH [AEGYO VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/VmfJQmQuQzA',
   },
   {
     id: '4',
-    title: 'title4',
-    category: 'interview',
-    url: 'https://youtu.be/NVzV6A0Xfes',
+    title:
+      '킹덤(KINGDOM) BLACK CROWN 내가 살고싶은 왕국이로소이다..!! [ORIGINAL VER. PERFORMANCE] ',
+    category: 'pinedoll',
+    url: 'https://youtu.be/kEUqpiiDm0E',
   },
   {
     id: '5',
-    title: 'title5',
-    category: 'washing',
-    url: 'https://youtu.be/sDcWpbkyxu4',
+    title:
+      '킹덤(KINGDOM) BLACK CROWN 하트 받으러 오세요오오오❤️[HEART VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/RTX5gyQnbKA',
   },
   {
     id: '6',
-    title: 'title6',
+    title: '킹덤(KINGDOM) BLACK CROWN [COMIC VER. PERFORMANCE]',
     category: 'pinedoll',
-    url: 'https://youtu.be/amOSaNX7KJg',
+    url: 'https://youtu.be/1sGE3y3hnXc',
+  },
+  {
+    id: '8',
+    title:
+      '다크비(DKB) 왜만나(rollercoaster), 잘 생겼으니까 만나지 ㅋㅋ [ORIGINAL VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/KTJiQWUluUw',
+  },
+  {
+    id: '9',
+    title:
+      '다크비(DKB) 왜 안 만나?ㅋ(rollercoaster) 머리부터 발끝까지 하트💗💗💗 [HEART VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/26oeQ9lJAV0',
+  },
+  {
+    id: '10',
+    title:
+      '스우파는 끝났지만ㅜ 다크비(DKB) 왜만나(rollercoaster) 댄스덕에 😍 [BATTLE VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/9YklMEgytRI',
+  },
+  {
+    id: '12',
+    title:
+      '(양해구함🙏) 블리처스(BLITZERS)가 실수좀할게 🤣 [ORIGINAL VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/tqtcrYm05Rw',
+  },
+  {
+    id: '13',
+    title:
+      '잘생긴 남자 애교 모음집😍 블리처스(BLITZERS) 실수좀할게😁 [AEGYO VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/zrf3VIwySQQ',
+  },
+
+  {
+    id: '15',
+    title:
+      '블리처스(BLITZERS) 내적댄스(HOP-IN)🕺💃 올해 젤귀엽 ㅇㅈ [ORIGINAL VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/hM90szJuh-M',
+  },
+  {
+    id: '16',
+    title:
+      '블리처스(BLITZERS) 쉘위댄스? 🎵 연말파티원 급구(1/n) [SOLO PERFORMANCE] ',
+    category: 'pinedoll',
+    url: 'https://youtu.be/26sNwVeglTI',
+  },
+];
+const tempDataList1 = [
+  {
+    id: '8',
+    title:
+      '다크비(DKB) 왜만나(rollercoaster), 잘 생겼으니까 만나지 ㅋㅋ [ORIGINAL VER. PERFORMANCE]',
+    category: '세탁기',
+    url: 'https://youtu.be/KTJiQWUluUw',
+  },
+  {
+    id: '9',
+    title:
+      '다크비(DKB) 왜 안 만나?ㅋ(rollercoaster) 머리부터 발끝까지 하트💗💗💗 [HEART VER. PERFORMANCE]',
+    category: '세탁기',
+    url: 'https://youtu.be/26oeQ9lJAV0',
+  },
+  {
+    id: '10',
+    title:
+      '스우파는 끝났지만ㅜ 다크비(DKB) 왜만나(rollercoaster) 댄스덕에 😍 [BATTLE VER. PERFORMANCE]',
+    category: '세탁기',
+    url: 'https://youtu.be/9YklMEgytRI',
+  },
+];
+const tempDataList3 = [
+  {
+    id: '14',
+    title: '블리처스(BLITZERS) ☃ 올겨울 필수 아이(돌)템 BEST7 [TMI]',
+    category: 'interview',
+    url: 'https://youtu.be/-DfZ2UWl9ro',
+  },
+  {
+    id: '2',
+    title: '이걸 다 말해도 되는 거야? 스카이리(SKYLE) TMI',
+    category: 'interview',
+    url: 'https://youtu.be/giMGCfay7Os',
+  },
+  {
+    id: '7',
+    title: '킹덤(KINGDOM) 여자가 좋아하는 남자유형 다있음 [TMI]',
+    category: 'interview',
+    url: 'https://youtu.be/VG2mQXoRS2A',
+  },
+  {
+    id: '11',
+    title: '수능 끝났는데 남소 받을래? (기대해 9명임😊) 다크비(DKB)랑 [TMI]',
+    category: 'interview',
+    url: 'https://youtu.be/FIImoLz-UQ4',
+  },
+];
+const tempDataList2 = [
+  {
+    id: '1',
+    title:
+      '오늘부터 최애, 스카이리(SKYLE) FLY UP HIGH [HEART VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/58WPGNcA6DM',
+  },
+
+  {
+    id: '3',
+    title:
+      '데뷔해줘서 고마운 ㅜㅜ 스카이리(SKYLE) FLY UP HIGH [AEGYO VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/VmfJQmQuQzA',
+  },
+  {
+    id: '4',
+    title:
+      '킹덤(KINGDOM) BLACK CROWN 내가 살고싶은 왕국이로소이다..!! [ORIGINAL VER. PERFORMANCE] ',
+    category: 'pinedoll',
+    url: 'https://youtu.be/kEUqpiiDm0E',
+  },
+  {
+    id: '5',
+    title:
+      '킹덤(KINGDOM) BLACK CROWN 하트 받으러 오세요오오오❤️[HEART VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/RTX5gyQnbKA',
+  },
+  {
+    id: '6',
+    title: '킹덤(KINGDOM) BLACK CROWN [COMIC VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/1sGE3y3hnXc',
+  },
+  {
+    id: '8',
+    title:
+      '다크비(DKB) 왜만나(rollercoaster), 잘 생겼으니까 만나지 ㅋㅋ [ORIGINAL VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/KTJiQWUluUw',
+  },
+  {
+    id: '9',
+    title:
+      '다크비(DKB) 왜 안 만나?ㅋ(rollercoaster) 머리부터 발끝까지 하트💗💗💗 [HEART VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/26oeQ9lJAV0',
+  },
+  {
+    id: '10',
+    title:
+      '스우파는 끝났지만ㅜ 다크비(DKB) 왜만나(rollercoaster) 댄스덕에 😍 [BATTLE VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/9YklMEgytRI',
+  },
+  {
+    id: '12',
+    title:
+      '(양해구함🙏) 블리처스(BLITZERS)가 실수좀할게 🤣 [ORIGINAL VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/tqtcrYm05Rw',
+  },
+  {
+    id: '13',
+    title:
+      '잘생긴 남자 애교 모음집😍 블리처스(BLITZERS) 실수좀할게😁 [AEGYO VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/zrf3VIwySQQ',
+  },
+
+  {
+    id: '15',
+    title:
+      '블리처스(BLITZERS) 내적댄스(HOP-IN)🕺💃 올해 젤귀엽 ㅇㅈ [ORIGINAL VER. PERFORMANCE]',
+    category: 'pinedoll',
+    url: 'https://youtu.be/hM90szJuh-M',
+  },
+  {
+    id: '16',
+    title:
+      '블리처스(BLITZERS) 쉘위댄스? 🎵 연말파티원 급구(1/n) [SOLO PERFORMANCE] ',
+    category: 'pinedoll',
+    url: 'https://youtu.be/26sNwVeglTI',
   },
 ];
 const PineClipContainer = props => {
   const [category, setCategory] = useState(0);
-
+  const [videoList, setVideoList] = useState(tempDataList);
+  const [title, setTitle] = useState('');
+  const [videoUrl, setVideoUrl] = useState('');
+  const handlerCategory = value => {
+    setCategory(value);
+  };
+  useEffect(() => {
+    if (category === 1) {
+      setVideoList(tempDataList1);
+    } else if (category === 2) {
+      setVideoList(tempDataList2);
+    } else if (category === 3) {
+      setVideoList(tempDataList3);
+    } else {
+      setVideoList(tempDataList);
+    }
+  }, [category]);
+  const videoPanel = useRef();
+  const [isBottom, setIsBottom] = useState(true);
+  const openVideoPlayer = (url, titleName) => {
+    // setVideoUrl(url);
+    setVideoUrl(url.substring(url.lastIndexOf('/') + 1));
+    console.log(url.substring(url.lastIndexOf('/') + 1));
+    setTitle(titleName);
+    setIsBottom(false);
+    videoPanel.current.show();
+  };
   return (
     <PineClipPresenter
       {...props}
-      videoList={tempData}
+      videoList={videoList}
       category={category}
       setCategory={setCategory}
+      handlerCategory={handlerCategory}
+      videoPanel={videoPanel}
+      isBottom={isBottom}
+      setIsBottom={setIsBottom}
+      openVideoPlayer={openVideoPlayer}
+      videoUrl={videoUrl}
     />
   );
 };
--- src/Screens/PineClip/PineClipPresenter.js
+++ src/Screens/PineClip/PineClipPresenter.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import {Box, FlatList, HStack} from 'native-base';
+import {Box, Center, FlatList, HStack, Image, Text} from 'native-base';
 
 import MenuComponent from '../../Components/MenuComponent';
 import Gbutton from '../../Components/GbuttonComponent';
 import VideoBox from '../../Components/VideoBoxComponent';
-
+import SlidingUpPanel from 'rn-sliding-up-panel';
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
+import {
+  fontSizePersentage,
+  heightPersentage,
+  widthPersentage,
+  YouTubeAPIKey,
+} from '../../Commons/CommonUtil';
+import {TouchableOpacity} from 'react-native';
+import ArrowDownIcon from '../../Assets/Image/icon_musicplayer_arrow_down.png';
+import YouTube from 'react-native-youtube';
+const youtubeApiKey = YouTubeAPIKey();
 const PineClipPresenter = props => {
   return (
     <Box flex={1}>
@@ -24,7 +39,7 @@ const PineClipPresenter = props => {
             fs={13}
             fw={'500'}
             rounded={4}
-            onPress={() => props.setCategory(1)}
+            onPress={() => props.handlerCategory(1)}
           />
           <Gbutton
             wp={70}
@@ -35,7 +50,7 @@ const PineClipPresenter = props => {
             fs={13}
             fw={'500'}
             rounded={4}
-            onPress={() => props.setCategory(2)}
+            onPress={() => props.handlerCategory(2)}
           />
           <Gbutton
             wp={130}
@@ -46,7 +61,7 @@ const PineClipPresenter = props => {
             fs={13}
             fw={'500'}
             rounded={4}
-            onPress={() => props.setCategory(3)}
+            onPress={() => props.handlerCategory(3)}
           />
         </HStack>
         <FlatList
@@ -64,7 +79,7 @@ const PineClipPresenter = props => {
                 title={item.title}
                 participant={item.category}
                 videoUrl={item.url}
-                onPress={() => {}}
+                onPress={() => props.openVideoPlayer(item.url, item.title)}
                 wp={320}
                 hp={230}
               />
@@ -73,6 +88,68 @@ const PineClipPresenter = props => {
           keyExtractor={item => item.id}
         />
       </Box>
+      <SlidingUpPanel
+        ref={props.videoPanel}
+        allowDragging={false}
+        backdropOpacity={0.98}
+        friction={0.01}
+        draggableRange={{
+          top: responsiveHeight(heightPersentage(600)),
+          bottom: responsiveHeight(heightPersentage(0)),
+        }}
+        onMomentumDragStart={() => props.setIsBottom(false)}
+        onBottomReached={() => props.setIsBottom(true)}
+        showBackdrop={false}>
+        {props.isBottom ? (
+          <></>
+        ) : (
+          <Box
+            style={{
+              width: '100%',
+              height: responsiveHeight(heightPersentage(340)),
+            }}>
+            <Center>
+              <Box bgColor="white" pt={3} w="100%">
+                <HStack px={5} alignItems={'center'}>
+                  <Text
+                    color={'#0a0b0c'}
+                    fontSize={responsiveFontSize(fontSizePersentage(24))}
+                    noOfLines={1}
+                    style={{width: responsiveWidth(widthPersentage(300))}}>
+                    {props.title}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() => props.videoPanel.current.hide()}
+                    style={{position: 'absolute', right: 20}}>
+                    <Image
+                      source={ArrowDownIcon}
+                      alt={' '}
+                      style={{
+                        width: responsiveWidth(widthPersentage(24)),
+                        height: responsiveHeight(heightPersentage(24)),
+                      }}
+                      resizeMode={'contain'}
+                    />
+                  </TouchableOpacity>
+                </HStack>
+              </Box>
+              <Box bgColor="white" p={3} w="100%">
+                <YouTube
+                  videoId={props.videoUrl}
+                  apiKey={youtubeApiKey}
+                  play={false}
+                  fullscreen={false}
+                  loop={false}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                  }}
+                />
+              </Box>
+            </Center>
+          </Box>
+        )}
+      </SlidingUpPanel>
     </Box>
   );
 };
