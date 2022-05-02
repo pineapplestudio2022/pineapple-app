90f6e921180204dd2ae543074dc3c8dc7a58a86d
yuno <yhkim@idealbloom.io>
2022-05-02 14:36:19 +0900

    updateBgmStudio formdata 형식으로 업로드 변경

--- src/Screens/BgmStudio/BgmStudioContainer.js
+++ src/Screens/BgmStudio/BgmStudioContainer.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React, {useContext, useState} from 'react';
 import {launchImageLibrary} from 'react-native-image-picker';
 
@@ -37,6 +36,14 @@ const BgmStudioContainer = props => {
       defaultAlertMessage('이미지를 선택해주세요');
       return;
     }
+    if (keywordList.length === 0) {
+      defaultAlertMessage('키워드를 1개이상 등록해주세요');
+      return;
+    }
+    if (whereUse === null || whereUse === '' || whereUse === undefined) {
+      defaultAlertMessage('사용처를 입력해주세요');
+      return;
+    }
     setLoading(true);
 
     const formData = new FormData();
@@ -45,30 +52,24 @@ const BgmStudioContainer = props => {
       name: image.name,
       type: image.type,
     });
-
-    const result = await axios({
-      method: 'post',
-      url: 'http://3.37.96.74:9779/pineapple/api',
-      headers: {'content-type': 'multipart/form-data'},
-      data: formData,
+    keywordList.forEach((item, index) => {
+      formData.append(`keyword[${index}]`, item);
     });
-
-    if (result?.data) {
-      const s3Url = result?.data.data.toString();
-      console.log(`s3Url : ${s3Url}`);
-      const payload = {
-        keyword: keywordList,
-        whereUse: whereUse.toString(),
-        url: s3Url.toString(),
-        userId: userId.toString(),
-      };
-      const res = await APIKit.post('/bgmStudio/updateBgmStudio', payload);
-      if (res?.data?.IBcode === '1000') {
-        setLoading(false);
-        setBgmResult(true);
-      } else {
-        setLoading(false);
-      }
+    formData.append('whereUse', whereUse);
+    formData.append('userId', userId);
+    console.log(formData);
+    const result = await APIKit.post('/bgmStudio/updateBgmStudio', formData, {
+      headers: {'content-type': 'multipart/form-data'},
+    });
+    console.log(result);
+    if (result?.data.IBcode === '1000') {
+      setLoading(false);
+      setBgmResult(true);
+    } else if (result?.data.IBcode === '6000') {
+      defaultAlertMessage('잠시 후 다시 시도해주세요');
+      setLoading(false);
+    } else {
+      setLoading(false);
     }
 
     // if (result?.data) {
